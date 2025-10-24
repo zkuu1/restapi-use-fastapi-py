8588d15dafbb0ce75e290b202f2d198ef4d0c87f
Zkuu <142339560+zkuu1@users.noreply.github.com>
2025-10-24 18:40:29 +0700

    [+] Add Fetch API

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -16,3 +18,46 @@ def about():
             {
                 'about page'
             }}
+
+# limit query
+@app.get('/blog')
+def blog(limit=10, published: bool = True, sort: Optional[str] = None):
+
+    if published: 
+        return {'data' : f'{limit} published blog from database'}
+    else:
+        return {'data' : f'{limit} blog from database'}
+    
+ 
+
+# fetch data by id 
+@app.get('/blog/{id}')
+def show(id:int):
+    return {'data': id}
+
+# unpublished
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 
+            {"unpublished"}
+            }
+
+# fetch comment by id
+@app.get('/blog/{id}/comments')
+def comments():
+    return {'comment': 
+            {'1', '2'}
+            }
+
+
+# MODELS
+class Blog(BaseModel):
+    title: str
+    body: str
+    published_at: Optional[bool] = None  
+
+
+
+@app.post('/blog')
+def create_blog(request: Blog):
+    return {'data' : f'Blog is created with title as {request.title}'}
